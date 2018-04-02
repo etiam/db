3c09a5b962db61f626738d395723c835537de254
etiam <rosson@gmail.com>
2018-04-02 14:03:19 -0700

    WIP Travis ci

--- dependencies.sh
+++ dependencies.sh
@@ -5,5 +5,3 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 
 sudo apt-get update -qq
 sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang-5.0
-
-git submodule update --init
