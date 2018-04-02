c4e13574f2dbd95e58fbbf3d452c30d7f430ae72
etiam <rosson@gmail.com>
2018-04-02 13:04:37 -0700

    WIP Travis ci

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update -qq
-sudo apt-get install -y -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem1.58-dev libboost-python1.58-dev libboost-program-options1.58-dev python-dev llvm-5.0 clang-5.0
+sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang-5.0
 
 git submodule update --init
