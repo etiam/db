fc2a33caa5f145ef018b9a13391764ba46346546
etiam <rosson@gmail.com>
2018-04-03 02:02:32 -0700

    WIP Travis ci

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,11 @@
 
 # add for llvm-5.0
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-sudo apt-add-repository "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-5.0 main"
+sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main"
 
 sudo apt-get update -qq
-sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 autoconf-archive libgnutls28-dev 
+sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang++-5.0 
+
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-5.0 50 \
+--slave /usr/bin/clang++ clang++ /usr/bin/clang++-5.0
+sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-5.0 50
