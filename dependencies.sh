2af7185ad6748ace3af14a5e828e7d033a75b6df
etiam <rosson@gmail.com>
2018-05-10 16:07:19 -0700

    updated libut dependencies for travis.

--- dependencies.sh
+++ dependencies.sh
@@ -10,7 +10,7 @@ sudo apt-get update -qq
 sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang++-5.0 
 
 # for libut
-sudo apt-get install -qq libboost-iostreams-dev libboost-test-dev binutils-dev
+sudo apt-get install -qq libboost-iostreams-dev libboost-test-dev binutils-dev libilmbase-dev libglm-dev
 
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-5.0 50 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-5.0
 sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-5.0 50
