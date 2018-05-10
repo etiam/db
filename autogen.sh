1497216e3865e251784ea8aa34640e265bf90845
etiam <rosson@gmail.com>
2018-05-09 22:21:41 -0700

    More libut subdir work.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# don't fuck with this!
+git submodule update --init
 autoreconf --force --verbose --install --warnings=all
+
+# from the docs it looks like AX_SUBDIRS_CONFIGURE should work
+# like AC_CONFIG_SUBDIRS and make the following unneccesary but nope.
+pushd src/external/libut
+./autogen.sh
+popd
--- dependencies.sh
+++ dependencies.sh
@@ -5,8 +5,12 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main"
 
 sudo apt-get update -qq
+
+# for db
 sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang++-5.0 
 
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-5.0 50 \
---slave /usr/bin/clang++ clang++ /usr/bin/clang++-5.0
+# for libut
+sudo apt-get install -qq libboost-iostreams-dev libboost-test-dev binutils-dev
+
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-5.0 50 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-5.0
 sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-5.0 50
