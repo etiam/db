9aa400b5bdb9bf541d2a0511db67e6dc65f4b6d8
etiam <rosson@gmail.com>
2018-05-11 14:31:18 -0700

    Travis ci stuff for libut.

--- dependencies.sh
+++ dependencies.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-locale-gen en_US en_US.UTF-8
+sudo apt-get update -qq
+sudo apt-get install -qq locales
+/usr/sbin/locale-gen en_US en_US.UTF-8
 dpkg-reconfigure locales
 
 # add for llvm-5.0
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main"
 
-sudo apt-get update -qq
-
 # for db
-sudo apt-get install -qq qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang++-5.0 
-
+sudo apt-get install -qq libtool qt5-default qtbase5-dev libqt5webkit5-dev libclang-5.0-dev libboost-filesystem-dev libboost-python-dev libboost-program-options-dev python-dev llvm-5.0 clang++-5.0 
 # for libut
 sudo apt-get install -qq libboost-iostreams-dev libboost-test-dev binutils-dev libilmbase-dev libglm-dev
 
