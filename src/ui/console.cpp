af4d214be9cc8ddf40d759eb6212409ccefe875b
etiam <rosson@gmail.com>
2018-08-02 21:30:40 -0700

    ConsoleInput work.

--- src/ui/console.cpp
+++ src/ui/console.cpp
@@ -9,7 +9,10 @@
 # include "config.h"
 #endif
 
+#include <iostream>
+
 #include <QVBoxLayout>
+#include <QProxyStyle>
 
 #include "core/global.h"
 #include "core/signal.h"
@@ -19,6 +22,65 @@
 
 #include "output.h"
 #include "console.h"
+
+namespace
+{
+
+class ConsoleInput : public HistoryLineEdit
+{
+  public:
+    explicit ConsoleInput(QWidget *parent = nullptr);
+
+  protected:
+    void keyPressEvent(QKeyEvent *) override;
+
+  private:
+    std::unique_ptr<QStyle> m_style;
+};
+
+class LineEditStyle : public QProxyStyle
+{
+  public:
+    LineEditStyle(QStyle *style = 0) : QProxyStyle(style) { }
+
+    int pixelMetric(PixelMetric metric, const QStyleOption *option = 0, const QWidget *widget = 0) const;
+};
+
+ConsoleInput::ConsoleInput(QWidget *parent) :
+    HistoryLineEdit(parent)
+{
+    setFont(QFontDatabase::systemFont(QFontDatabase::FixedFont));
+
+    m_style = std::make_unique<LineEditStyle>(style());
+    setStyle(m_style.get());
+
+    setText("(gdb) ");
+}
+
+int
+LineEditStyle::pixelMetric(PixelMetric metric, const QStyleOption *option, const QWidget *widget) const
+{
+    if (metric == QStyle::PM_TextCursorWidth)
+        return 7;
+
+    return QProxyStyle::pixelMetric(metric, option, widget);
+}
+
+
+void ConsoleInput::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key())
+    {
+        case Qt::Key_Backspace:
+            if (cursorPosition() < 7)
+                break;
+
+        default:
+            HistoryLineEdit::keyPressEvent(event);
+    }
+}
+
+}
 
 namespace Ui
 {
@@ -31,7 +93,7 @@ Console::Console(QWidget *parent, bool editable) :
     m_output = new Output(this);
     m_output->setObjectName("consoleoutput");
 
-    m_lineedit = new HistoryLineEdit(this);
+    m_lineedit = new ConsoleInput(this);
     m_lineedit->setObjectName("consolelineedit");
 
     auto layout = new QVBoxLayout(this);
